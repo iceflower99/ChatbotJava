8896f8348746c73f3ad562109645b864620993b0
iceflower99 <mhal4767@740s-pl-120.local>
2015-10-21 09:10:46 -0600

    added new stuff

--- src/chat/controler/ChatController.java
+++ src/chat/controler/ChatController.java
@@ -1,12 +1,16 @@
 package chat.controler;
 
+import chat.veiw.ChatView;
+
 /**
  * Application controller for the Chatbot project.
  * @author mhal4767
- *@version 1.0 10/21/15
+ *@version 1.11 10/21/15 fixed error ;)
  */
 
 public class ChatController {
+	
+	private ChatView myDisplay;
 	
 	public void start()
 	{
---
+++ src/chat/veiw/ChatView.java
@@ -0,0 +1,16 @@
+package chat.veiw;
+
+import javax.swing.JOptionPane;
+
+public class ChatView 
+{
+public  String grabInput (String dispalyText)
+{
+	String returnedText="";
+	
+	return returnedText;
+}
+	
+
+
+}

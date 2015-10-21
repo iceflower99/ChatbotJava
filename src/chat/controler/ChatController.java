444fa5c7dc4375a30c3d5ee45faaa58690d44424
iceflower99 <mhal4767@740s-pl-120.local>
2015-10-21 08:33:04 -0600

    added to the repo

---
+++ src/chat/controler/ChatController.java
@@ -0,0 +1,16 @@
+package chat.controler;
+
+/**
+ * Application controller for the Chatbot project.
+ * @author mhal4767
+ *@version 1.0 10/21/15
+ */
+
+public class ChatController {
+	
+	public void start()
+	{
+		
+	}
+
+}
---
+++ src/chat/controler/chatRunner.java
@@ -0,0 +1,20 @@
+package chat.controler;
+
+/**
+ * Starts the Chatbot Program
+ * @author mhal4767
+ * @version 1.0 10-21-15
+ *
+ */
+
+public class chatRunner {
+	
+	public static void main (String []args)
+	{
+		ChatController myChatController = new ChatController();
+		 myChatController.start();
+		
+		
+	}
+
+}

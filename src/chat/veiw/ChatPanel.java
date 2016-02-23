8d864121c66127f6d0e0c71fc87bf4c1ff34c751
iceflower99 <mhal4767@740s-pl-1280.local>
2016-02-23 09:37:52 -0700

    updated chat panel

--- src/chat/veiw/ChatPanel.java
+++ src/chat/veiw/ChatPanel.java
@@ -3,12 +3,15 @@ package chat.veiw;
 import chat.controler.ChatController;
 import javax.swing.JPanel;
 import javax. swing.JButton;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.SpringLayout;
 import java.awt.event.*;
 import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
+
 public class ChatPanel extends  JPanel
 {
    private ChatController baseController;
@@ -17,6 +20,10 @@ public class ChatPanel extends  JPanel
    private SpringLayout baseLayout;
    private JLabel promptLabel;
    private JTextArea chatArea;
+   private JButton tweetButton;
+   private JButton loadButton;
+   private JScrollPane textPane;
+   
    public ChatPanel(ChatController baseController)
    {
 	   this.baseController=baseController;
@@ -25,27 +32,34 @@ public class ChatPanel extends  JPanel
 	   typingField = new JTextField("words can be typed here");
 	   firstButton=new JButton("Please don't click the button");
 	   
-	   
-	  
-	   
+	   setupChatPane();
 	   setupPanel();
 	   setupLayout();
 	   setupListeners();
    }
+   
+   private void setupChatPane()
+   {
+	
+	   textPane = new JScrollPane(chatArea);
+	   chatArea.setLineWrap(true);
+	   chatArea.setWrapStyleWord(true);
+	   chatArea.setEditable(false);
+	   textPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+	   textPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+   }
+   
    private void setupPanel()
    {
 	   this.setLayout(baseLayout);
 	   this.add(firstButton);
+	   this.add(textPane);
 	   this.add(typingField);
-	  
 	   }
    
    private void setupLayout()
    {
-	   baseLayout.putConstraint(SpringLayout.NORTH, typingField,36,SpringLayout.SOUTH,firstButton);
-	   baseLayout.putConstraint(SpringLayout.EAST, typingField, -127, SpringLayout.EAST, this);
-		baseLayout.putConstraint(SpringLayout.NORTH, firstButton, 64, SpringLayout.NORTH, this);
-		baseLayout.putConstraint(SpringLayout.WEST, firstButton, 58, SpringLayout.WEST, this);
+	  
    }
    private void setupListeners()
    {
